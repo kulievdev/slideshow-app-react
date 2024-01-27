f32eb3a28e451db0ccc4bd2da7a2d8477399dab7
Tohir Kuliev <kuliev.tech@gmail.com>
2024-01-27 15:03:57 -0500

    create slideshow page using react and styled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import kitchen from "./assets/kitchen.jpg";
+import tv from "./assets/tv.jpg";
+import nature from "./assets/nature.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { useState } from "react";
+import styled from "styled-components";
 
-export default App;
+const SlideshowContainer = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`;
+
+const SlideshowWrapper = styled.div`
+    max-width: 800px;
+    position: relative;
+    margin: 0 auto;
+`;
+
+const Slide = styled.div`
+    display: none;
+    transition: opacity 1.5s ease-in-out;
+
+    &.active {
+        display: block;
+    }
+
+    img {
+        width: 100%;
+        height: auto;
+    }
+`;
+
+const SlideshowButtons = styled.div`
+    position: relative;
+`;
+
+const NavigationButton = styled.button`
+    background-color: #333;
+    color: white;
+    border: none;
+    padding: 10px;
+    font-size: 16px;
+    cursor: pointer;
+
+    &.next {
+        position: absolute;
+        right: 0;
+    }
+`;
+
+// Main component
+const Slideshow = () => {
+    const images = [kitchen, tv, nature];
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const nextSlide = () => {
+        setCurrentSlide((prevSlide) => (prevSlide + 1) % images.length);
+    };
+
+    const prevSlide = () => {
+        setCurrentSlide(
+            (prevSlide) => (prevSlide - 1 + images.length) % images.length
+        );
+    };
+
+    return (
+        <SlideshowContainer>
+            <SlideshowWrapper>
+                {images.map((image, index) => (
+                    <Slide
+                        key={index}
+                        className={index === currentSlide ? "active" : ""}
+                    >
+                        <img src={image} alt={`Slide ${index + 1}`} />
+                    </Slide>
+                ))}
+                <SlideshowButtons>
+                    <NavigationButton className="previous" onClick={prevSlide}>
+                        Previous
+                    </NavigationButton>
+                    <NavigationButton className="next" onClick={nextSlide}>
+                        Next
+                    </NavigationButton>
+                </SlideshowButtons>
+            </SlideshowWrapper>
+        </SlideshowContainer>
+    );
+};
+
+export default Slideshow;
